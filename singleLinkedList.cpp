7ff463744bff04d9d943db633460c76f8dcf6d64
Mzahdul <seeglas8@gmail.com>
2026-04-21 11:22:12 +0700

    bool listEmpty

--- singleLinkedList.cpp
+++ singleLinkedList.cpp
@@ -20,5 +20,49 @@ public:
     }
 
     void addNode() 
+    {
+        int nim;
+        cout << "masukan nomer mahasiswa : ";
+        cin >> nim;
+
+        Node *nodeBaru = new Node;
+        nodeBaru->noMhs = nim;
+
+        if (START == NULL  || nim <= START->noMhs)
+        {
+            if (START != NULL && nim <= START->noMhs) 
+            {
+                cout << "\nDuplikasi noMhs tidak di ijinkan\n" << endl;
+                return;
+            }
+
+            nodeBaru->next = START;
+            START = nodeBaru;
+            return;
+        }
+            
+        Node *previous = START;
+        Node *current = START;
+
+        while (current != NULL && nim >= current->noMhs) 
+        {
+            if (nim == current->noMhs) 
+            {
+                cout << "\nDuplikasi noMHS tidak di ijinkan\n" << endl;
+                return;
+            }
+            previous = current;
+            current = current->next;
+        }
+
+        nodeBaru->next = current;
+        previous->next = nodeBaru;
+    }
+
+    bool listEmpty() 
+    {
+        return START == NULL;
+    }
+
     
 };
